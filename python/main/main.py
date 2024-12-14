8cb8fb5e83bd0b74b221b29dceb4074ad73e4d20
Manshilin Oleg <manshilin.o@gmail.com>
2024-12-14 19:02:25 +0300

    Error handling with exception instead of sys

--- python/main/main.py
+++ python/main/main.py
@@ -1,19 +1,12 @@
-import sys
-
 from days.day1 import day1_part1, day1_part2
 
 if __name__ == "__main__":
     days_dict = {1: [day1_part1, day1_part2]}
-    try:
-        choosen_day = int(input().strip())
-    except Exception as e:
-        print("No such day is available!")
-        print(e)
-        sys.exit(0)
+    choosen_day = int(input().strip())
     try:
         needed_funcs = days_dict[choosen_day]
         for fnc in needed_funcs:
             fnc()
     except Exception as e:
         print("Error occured. This day is either don't exists or unimplemented")
-        print(e)
+        print(f"Your input was parsed as {e}")
